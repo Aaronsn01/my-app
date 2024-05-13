aff05d466eaa129c81533fdd31765495b9476739
Aaronsn01 <aaronescuderon01@gmail.com>
2024-05-13 20:13:02 +0200

    Cambios Home

--- src/components/ui/home.tsx
+++ src/components/ui/home.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import boxerImage from '../img/crossfit-home.jpg'; // Asegúrate de cambiar esta ruta a la ubicación de tu imagen
-import IconNavbar from "@/components/ui/iconnavbar";
-import CrossfitHome from "../icons/crossfit-home";
+import boxerImage from "../img/crossfit-home.jpg"; // Asegúrate de cambiar esta ruta a la ubicación de tu imagen
+import Image from "next/image";
+import image from "../../../public/image.png";
+import Barra from "./nav-home";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 min-h-screen">
-      <div className="pt-40 pl-40">
-        <h1 className="text-6xl font-bold mt-40">
-          THRIVE IN THE <span className="text-primary">WILD</span>
+    <div className="relative min-h-screen">
+      <div className="absolute pt-40 pl-40">
+        <h1 className="flex flex-col text-8xl font-bold mt-40 gap-3">
+          THRIVE IN THE
+        </h1>
+        <h1>
+          <span className="text-primary text-8xl font-bold">WILD</span>
         </h1>
       </div>
-      <div className="flex h-full justify-center">
-        <CrossfitHome/>
+      <div className="flex justify-center">
+        <Image src={image} alt="imagen home" />
       </div>
-      <div>
-        <IconNavbar className="flex justify-center items-center"/>
+      <div className="flex justify-center ">
+        <Barra/>
       </div>
     </div>
   );
