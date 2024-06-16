879832d53fc7b73bbb1d73b8c40437c12abea26a
Aaronsn01 <aaronescuderon01@gmail.com>
2024-06-16 19:20:29 +0200

    cambios

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,11 +4,11 @@ import Logo from "../icons/logo";
 
 export default function Header() {
   return (
-    <div className="h-20 md:h-[120px] w-full flex items-center justify-center md:justify-between p-5 bg-black text-white">
-      <div className="md:w-full w-40 h-28 flex justify-center md:h-auto sm:justify-start">
+    <div className="h-20 md:h-[120px] w-full flex items-center justify-center lg:justify-between p-5 bg-black text-white">
+      <div className="md:w-full w-40 h-28 flex justify-center md:h-auto lg:justify-start">
         <Logo />
       </div>
-      <nav className="hidden sm:flex flex-row items-center gap-10 text-lg">
+      <nav className="hidden lg:flex flex-row items-center gap-10 text-lg">
         <a href="#pricing">PRECIOS</a>
         <a href="#aboutus">NOSOTROS</a>
         <a href="#location">UBICACIÓN</a>
--- src/components/ui/home.tsx
+++ src/components/ui/home.tsx
@@ -7,21 +7,21 @@ export default function Home() {
   return (
     <div className="relative min-h-screen">
       <div className="absolute p-4 sm:p-10 lg:pt-48 lg:pl-32 z-50 flex flex-col gap-4">
-        <div className="text-xl sm:text-5xl md:text-6xl lg:text-8xl font-bold">
+        <div className="text-2xl md:text-4xl lg:text-8xl font-bold">
           <p className="flex flex-col gap-3">DESCUBRE TU</p>
           <p>
             <span className="text-primary">FUERZA</span>
             <span> INTERIOR</span>
           </p>
         </div>
-        <div className="text-[8px] sm:text-2xl lg:text-2xl font-bold">
+        <div className="text-[10px] md:text-xl lg:text-2xl font-bold">
           <p className="flex flex-col gap-3">ÚNETE A LA TRIBU FITNESS EN MIRAMADRID</p>
         </div>
       </div>
       <div className="flex justify-center relative">
         <Image src={image} alt="imagen home" className="w-full h-1/2 md:h-auto object-cover"/>
-        <div className="flex justify-center items-end absolute w-full h-full mt-[145px] md:mt-10">
-          <Barra className="px-4 sm:px-4"/>
+        <div className="flex justify-center items-end absolute w-full h-full mt-[150px] md:mt-32 lg:mt-10">
+          <Barra className="px-4"/>
         </div>
       </div>
     </div>
