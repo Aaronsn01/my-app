50206745fc6538305c0eee6a70b3a8508567be95
Aaronsn01 <aaronescuderon01@gmail.com>
2024-06-17 17:24:17 +0200

    Metadato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 import Footer from "../components/ui/footer";
 import Header from "../components/ui/header";
 import { Toaster } from "../components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -13,11 +13,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="UHHcV3rhAWBM9TdxF7B22Yyky3Q9RC3dil7sh7WWZ5Q" />
+      </head>
       <body className={cn("bg-customGray font-sans antialiased")}>
-          <Header />
-          {children}
-          <Footer />
-          <Toaster />
+        <Header />
+        {children}
+        <Footer />
+        <Toaster />
+        <Analytics />
       </body>
     </html>
   );
